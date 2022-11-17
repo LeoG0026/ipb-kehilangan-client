f9ba060d44acd9980788e8ab602209a8e3026a11
LeoG0026 <79181559+LeoG0026@users.noreply.github.com>
2022-11-18 00:40:05 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Homepage from './Homepage';
-import Homepage from './Login';
-import Homepage from './Register';
+import Login from './Login';
+import Register from './Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
